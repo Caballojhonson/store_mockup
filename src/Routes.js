60ad29f7ddd511abcb9efc71079e5c654a3fd1cb
caballo <imanol.l.p@hotmail.com>
2021-11-01 15:49:06 +0100

    Third attempt on fixing gh-pages / react-router-dom compatibility

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import Homepage from './Pages/Homepage';
 import Store from './Pages/Store';
 
 export default function Routes() {
 	return (
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<HashRouter >
 			<Switch>
 				<Route exact path="/" component={Homepage} />
                 <Route exact path="/app" component={App} />
                 <Route exact path="/store" component={Store} />
 			</Switch>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
